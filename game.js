e9979f6eaac8c8c0a1e63ad0d24246a01337bc92
Being Yuvraj <ronak41294@gmail.com>
2025-01-27 14:15:23 +0530

    Final commit: Completed Simon Game - added all features, final styling, and cleaned up code for final version.

--- game.js
+++ game.js
@@ -1,135 +1,118 @@
-// // alert("hello");
-// var buttonColours  = ["red", "blue", "green", "yellow"];
-// var gamePattern = [];
-// var userClickedPattern = [];
-// var started = false;
-// var level = 0;
-
-// $(document).on("keypress",function(){
-//   if(!started){
-//     $("#level-title").text("Level: " + level);
-    
-//     nextSequence(level);
-//     started = true;
-//     // $(document).off("keypress");
-//   }
-
-
- 
-
-// });
-
-// $(".btn").on("click", function() {
-//   var userChosenColour = $(this).attr("id");
-//   userClickedPattern.push(userChosenColour);
-//   // console.log(userClickedPattern);
-//   playSound(userChosenColour);
-//   animatePress(userChosenColour);
-
-// });
-// function nextSequence(){
-
-//   level++;
-
-//   $("#level-title").text("Level: " + level);
-
-//   var randomNumber =  Math.floor(Math.random()*4);
-//  //  return randomNumber;
-//   var randomChosencolour = buttonColours[randomNumber];
-// // console.log(randomChosencolour);
-// gamePattern.push(randomChosencolour);
-// $("#" + randomChosencolour).fadeOut(100).fadeIn(100);
-// playSound(randomChosencolour);
-
-
-// // console.log("Level: " + level);
-
-// }
-
-// function animatePress(currentColour){
-//   $("#" + currentColour).addClass("pressed");
-//   setTimeout(function(){
-//     $("#" + currentColour).removeClass("pressed");
-//   }, 100);
-// }
-
-
-
-
-// // $("#" + randomChosencolour).on("click", function(){
- 
-// // });
-// // $("#" + randomChosencolour).on("click", function(){
-// // var sound = new Audio("./sounds/"+randomChosencolour+".mp3");
-// //   iiiiiisound.play();
-// // });
-
-// function playSound(name){
-//   var sound = new Audio("./sounds/"+name+".mp3");
-//   sound.play();
-// }
-
-
-
-
-var buttonColours = ["red", "blue", "green", "yellow"];
-
+// alert("hello");
+var buttonColours  = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
-
-//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
 var started = false;
+var level = 0;function checkAnswer(currentLevel) {
 
-//2. Create a new variable called level and start at level 0.
-var level = 0;
+  //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
-//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
-$(document).keypress(function() {
-  if (!started) {
+    console.log("success");
 
-    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
-    $("#level-title").text("Level " + level);
+    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+    if (userClickedPattern.length === gamePattern.length){
+
+      //5. Call nextSequence() after a 1000 millisecond delay.
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+
+    }
+
+  } else {
+
+    console.log("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over")
+
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+
+
+  }
+
+}
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  started = false;
+
+}
+
+
+
+$(document).on("keypress",function(){
+  if(!started){
+    $("#level-title").text("Level: " + level);
+    
     nextSequence();
     started = true;
+    // $(document).off("keypress");
   }
+
+
+ 
+
 });
 
-$(".btn").click(function() {
-
+$(".btn").on("click", function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-
+  // console.log(userClickedPattern);
+  checkAnswer(userClickedPattern.length-1);
+  
   playSound(userChosenColour);
   animatePress(userChosenColour);
+
 });
+function nextSequence(){
 
-function nextSequence() {
-
-  //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
   level++;
 
-  //5. Inside nextSequence(), update the h1 with this change in the value of level.
-  $("#level-title").text("Level " + level);
+  $("#level-title").text("Level: " + level);
 
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
+  var randomNumber =  Math.floor(Math.random()*4);
+ //  return randomNumber;
+  var randomChosencolour = buttonColours[randomNumber];
+// console.log(randomChosencolour);
+gamePattern.push(randomChosencolour);
+// $("#" + randomChosencolour).fadeOut(100).fadeIn(100);/
+playSound(randomChosencolour);
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColour);
+
+// console.log("Level: " + level);
+
 }
 
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
-
-function animatePress(currentColor) {
-  $("#" + currentColor).addClass("pressed");
-  setTimeout(function () {
-    $("#" + currentColor).removeClass("pressed");
+function animatePress(currentColour){
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
   }, 100);
 }
+
+
+
+
+// $("#" + randomChosencolour).on("click", function(){
+ 
+// });
+// $("#" + randomChosencolour).on("click", function(){
+// var sound = new Audio("./sounds/"+randomChosencolour+".mp3");
+//   iiiiiisound.play();
+// });
+
+function playSound(name){
+  var sound = new Audio("./sounds/"+name+".mp3");
+  sound.play();
+}
+
+
+
 
 
   
